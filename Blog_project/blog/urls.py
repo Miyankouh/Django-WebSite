80708f730ea3216630052cf7fcb9b0f3790f6133
Miyankouh <majid.miyankouh@gmail.com>
2022-04-14 14:26:53 +0430

    add template-style-static to Blog project

--- Blog_project/blog/urls.py
+++ Blog_project/blog/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='starting-page'),
     path('posts', views.posts, name='posts-page'),
-    path('posts/<slug:slug>', views.single_post, name='post-detail-page'), # toplearn.com/posts/<slug>
-    path(),
+    path('posts/<slug:slug>', views.single_post, name='post-detail-page'), 
 ]
