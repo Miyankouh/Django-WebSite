a013fed371dfcf4497ae3a237c0d3370330858a3
Miyankouh <majid.miyankouh@gmail.com>
2022-04-16 14:17:19 +0430

    Display database information on the page

--- eshop_project/product/views.py
+++ eshop_project/product/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from django.http import Http404
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product/product_list.html', {
+        'products': products
+    })
+
+
+def product_detail(request, product_id):
+    # try:
+        # product = Product.objects.get(id=product_id)
+    # except:
+        # raise Http404()
+    
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product/product_detail.html', {
+        'product': product
+    })
