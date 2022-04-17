baa0923dcd19e3934a023350b733724204d27b62
Miyankouh <majid.miyankouh@gmail.com>
2022-04-17 16:26:06 +0430

    Add- template to project

--- eshop_project/eshop_project/urls.py
+++ eshop_project/eshop_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home_module.urls')),
+    path('prodoucts', include('product.urls')),
     path('admin/', admin.site.urls),
-    path('', include('product.urls')),
 ]
--- eshop_project/product/admin.py
+++ eshop_project/product/admin.py
@@ -7,6 +7,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'is_active', 'is_delete']
     list_editable = ['price', 'is_active']
 
+
 admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.ProductCategory)
 admin.site.register(models.ProductTag)
