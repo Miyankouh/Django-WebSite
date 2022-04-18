eb9dd930057c8ca7b1aebab56350409cc6afa76f
Miyankouh <majid.miyankouh@gmail.com>
2022-04-18 11:34:52 +0430

    Change in model and view and admin page

--- eshop_project/product/models.py
+++ eshop_project/product/models.py
@@ -4,12 +4,15 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+
 class ProductCategory(models.Model):
-    title = models.CharField(max_length=300, db_index=True, verbose_name='عنوان')
-    url_title = models.CharField(max_length=300, db_index=True, verbose_name='عنوان در url')
+    title = models.CharField(
+        max_length=300, db_index=True, verbose_name='عنوان')
+    url_title = models.CharField(
+        max_length=300, db_index=True, verbose_name='عنوان در url')
     is_active = models.BooleanField(verbose_name='فعال / غیر فعال')
     is_delete = models.BooleanField(verbose_name='حذف شده / نشده')
-    
+
     def __str__(self):
         return f"( {self.title} - {self.url_title})"
 
@@ -18,17 +21,35 @@ class ProductCategory(models.Model):
         verbose_name_plural = 'دسته بندی ها'
 
 
+class ProductBrand(models.Model):
+    title = models.CharField(
+        max_length=300, verbose_name='نام برند', db_index=True)
+    is_active = models.BooleanField(verbose_name='فعال / غیر فعال')
+
+    class Meta:
+        verbose_name = 'برند'
+        verbose_name_plural = 'برند ها'
+
+    def __str__(self):
+        return self.title
+
+
 class Product(models.Model):
     title = models.CharField(max_length=100, verbose_name='عنوان محصول')
     category = models.ManyToManyField(
-        ProductCategory, 
-        related_name='product_categories', 
+        ProductCategory,
+        related_name='product_categories',
         verbose_name='دسته بندی ها')
+    brand = models.ForeignKey(ProductBrand, on_delete=models.CASCADE, verbose_name='برند', null=True, blank=True)
     price = models.IntegerField(verbose_name='قیمت')
-    short_description = models.TextField(null=True, db_index=True , verbose_name='توضیح کوتاه')
-    description = models.TextField(null=True,  db_index=True, verbose_name='توضیح اصلی')
-    is_active = models.BooleanField(default=False, verbose_name='فعال / غیر فعال')
-    slug = models.SlugField(default='', null=False, db_index=True, blank=True, max_length=255, unique=True, verbose_name='عنوان در url')
+    short_description = models.TextField(
+        null=True, db_index=True, verbose_name='توضیح کوتاه')
+    description = models.TextField(
+        null=True,  db_index=True, verbose_name='توضیح اصلی')
+    is_active = models.BooleanField(
+        default=False, verbose_name='فعال / غیر فعال')
+    slug = models.SlugField(default='', null=False, db_index=True, blank=True,
+                            max_length=255, unique=True, verbose_name='عنوان در url')
     is_delete = models.BooleanField(verbose_name='حذف شده / نشده')
 
     def get_absolute_url(self):
@@ -40,19 +61,21 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.price})"
-    
+
     class Meta:
         verbose_name = ' محصول'
         verbose_name_plural = '  محصول ها'
 
 
 class ProductTag(models.Model):
-    caption = models.CharField(max_length=300, db_index=True, verbose_name='تگ')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_tags')
-    
+    caption = models.CharField(
+        max_length=300, db_index=True, verbose_name='تگ')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='product_tags')
+
     def __str__(self):
         return self.caption
 
     class Meta:
         verbose_name = 'تگ محصول'
-        verbose_name_plural ='تگ های محصولات'
+        verbose_name_plural = 'تگ های محصولات'
--- eshop_project/product/views.py
+++ eshop_project/product/views.py
@@ -14,11 +14,6 @@ def product_list(request):
 
 
 def product_detail(request, slug):
-    # try:
-        # product = Product.objects.get(id=product_id)
-    # except:
-        # raise Http404()
-    
     product = get_object_or_404(Product, slug=slug)
     return render(request, 'product/product_detail.html', {
         'product': product
