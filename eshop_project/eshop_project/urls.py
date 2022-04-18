1f60c3847419756db8bd48d59ea58eff8c73bd24
Miyankouh <majid.miyankouh@gmail.com>
2022-04-18 12:25:15 +0430

    Add the Contact app and set it up

--- eshop_project/eshop_project/urls.py
+++ eshop_project/eshop_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('home_module.urls')),
+    path('contact-us/', include('contact_module.urls')),
     path('prodoucts/', include('product.urls')),
     path('admin/', admin.site.urls),
 ]
