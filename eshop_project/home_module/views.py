411074475646da62240a1a710f072e2e38d8041c
Miyankouh <majid.miyankouh@gmail.com>
2022-04-19 13:49:12 +0430

    add- Class base view

--- eshop_project/home_module/views.py
+++ eshop_project/home_module/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.views.generic.base import TemplateView 
 # Create your views here.
 
-def index_page(request):
-    return render(request, 'home_module/index_page.html')
+# class HomeView(View):
+#     def get(self, request):
+#         context = {
+#             'data': 'this is data'
+#         }
+#         return render(request, 'home_module/index_page.html', context)
+
+class HomeView(TemplateView):
+   template_name = 'home_module/index_page.html'
+
+   def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = 'this is data'
+        return context
 
 
 def contact_page(request):
