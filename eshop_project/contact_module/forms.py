db35a62716505f49e4eb4e184fd84525d68ae825
Miyankouh <majid.miyankouh@gmail.com>
2022-04-19 10:45:00 +0430

    Add widget to forms

--- eshop_project/contact_module/forms.py
+++ eshop_project/contact_module/forms.py
@@ -2,8 +2,10 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
-    fullname = forms.CharField()
-    email = forms.EmailField()
-    subject = forms.CharField()
-    text = forms.CharField()
+    fullname = forms.CharField(label="نام و نام خانوادگی", max_length=50, error_messages={
+        'required':'لطفا نام و نام خانوادگی خود را وارد کنید'
+        })
+    email = forms.EmailField(label="ایمیل", widget=forms.EmailInput)
+    subject = forms.CharField(label="عنوان")
+    text = forms.CharField(label="متن پیام", widget=forms.Textarea)
 
