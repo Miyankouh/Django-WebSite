7768e3523a67454e13a97d03f03c5f75251f2e42
Miyankouh <majid.miyankouh@gmail.com>
2022-04-25 20:26:19 +0430

    Dynamic Article

--- eshop_project/article_module/views.py
+++ eshop_project/article_module/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.list import ListView 
+from article_module.models import Article
 
-# Create your views here.
+
+# class ArticleView(View):
+    # def get(self, request):
+        # articles = Article.objects.filter(is_active=True)
+        # context = {
+            # 'articles': articles
+        # }
+        # return render(request, 'article_module/articles_page.html', context)
+
+
+class ArticleListView(ListView):
+    model = Article
+    paginate_by = 5
+    template_name = 'article_module/articles_page.html'
