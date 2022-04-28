0ccb9fc8ce121a11faf60ba8a6d819eff591a2ad
Miyankouh <majid.miyankouh@gmail.com>
2022-04-28 15:42:27 +0430

    add- AJAX funtion

--- eshop_project/article_module/views.py
+++ eshop_project/article_module/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
@@ -46,3 +46,8 @@ def article_categories_component(request: HttpRequest):
         'main_categories': article_main_categories
     }
     return render(request, 'article_module/components/article_categories_component.html', context)
+
+
+def add_article_comment(request: HttpRequest):
+    print(request.GET)
+    return HttpResponse('helo')
