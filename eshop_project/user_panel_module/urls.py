38ee8cb58aac9d9ad649be0ae1627a66e838b784
Miyankouh <majid.miyankouh@gmail.com>
2022-04-30 13:10:53 +0430

    add- Change user password

--- eshop_project/user_panel_module/urls.py
+++ eshop_project/user_panel_module/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.UserPanelDashboardPage.as_view(), name='user_panel_dashboard'),
+    path('change-pass', views.ChangePasswordPage.as_view(), name='change_password_page'),
     path('edit-profile', views.EditUserProfilePage.as_view(), name='edit_profile_page')
 ]
