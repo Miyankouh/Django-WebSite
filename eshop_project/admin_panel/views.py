4a8177e60a718811c77ac2513bd8301179082b1a
miyankouh <majid.miyankouh@gmail.com>
2022-06-14 16:29:26 +0430

    add: views

--- eshop_project/admin_panel/views.py
+++ eshop_project/admin_panel/views.py
@@ -1,17 +1,20 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, UpdateView
 from article_module.models import Article
 
 # Create your views here.
-def all_articles(request: HttpRequest):
-    return render(request, 'admin_panel/articles_list.html')
+
+
+def index(request: HttpRequest):
+    return render(request, 'admin_panel/home/index.html')
 
 
 class ArticlesListView(ListView):
     model = Article
     paginate_by = 12
-    template_name = 'admin_panel/articles_list.html'
+    template_name = 'admin_panel/articles/articles_list.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(ArticlesListView, self).get_context_data(
@@ -25,3 +28,11 @@ class ArticlesListView(ListView):
             query = query.filter(
                 selected_categories__url_title__iexact=category_name)
         return query
+
+
+class ArticleEditView(UpdateView):
+    model = Article
+    template_name = 'admin_panel/articles/edit_article.html'
+    fields = '__all__'
+    success_url = reverse_lazy('admin_articles')
+    # success_url = 'admin-panel/articles/'
